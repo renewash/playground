94c299897b6b7b5d6a5b372bea43a189a8d9068d
renewash <ashton.lim@ultragreen.ai>
2026-03-10 15:05:55 +0800

    removed prev versions of drawable

--- apps/ui-1/src/components/konva/module/react/KonvaProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { type ReactNode } from "react";
-import { KonvaContext } from "./context";
-
-import { createDrawingEngine } from "../core/engine";
-
-export const KonvaProvider = ({ children }: { children: ReactNode }) => {
-  const engine = createDrawingEngine();
-  return <KonvaContext value={engine}>{children}</KonvaContext>;
-};
--- apps/ui-1/src/components/konva/module/react/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-import { type DrawingEngine } from "../core/types";
-
-export const KonvaContext = createContext<DrawingEngine | null>(null);
-export const useKonvaContext = () => {
-  const ctx = useContext(KonvaContext);
-  if (!ctx) throw new Error("Context needs to be used in provider");
-  return ctx;
-};
